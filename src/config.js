62f641937bf4c9fad0940f3566d9b9f34bfab796
oiramdev30 <oiramdev30@gmail.com>
2026-07-03 14:33:54 -0600

    Actualizar config.js para apuntar a Railway

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 // src/config.js
-// Cambia esta IP por la IP de tu computadora en la red local
-export const API_URL = 'http://192.168.1.72:8000/api/v1';
-export const API_BASE = 'http://192.168.1.72:8000';
+export const API_URL = 'https://life-os-backend-production-63db.up.railway.app/api/v1';
+export const API_BASE = 'https://life-os-backend-production-63db.up.railway.app';
